9a137b25f5c6eecf85b1f6e8ca761bf57586ac66
nimtego <nimtego@mail.ru>
2019-05-20 22:23:26 +0300

    PT-65[include_PT-60_PT-67]-Migrate_project_to_Kotlin   add:   - kotlin moxy implementation

---
+++ app/src/main/java/com/nimtego/plectrum/presentation/presenters/BasePresenter.kt
@@ -0,0 +1,22 @@
+package com.nimtego.plectrum.presentation.presenters
+
+import android.util.Log
+import com.arellomobile.mvp.MvpPresenter
+import com.nimtego.plectrum.presentation.mvp.BaseView
+
+abstract class BasePresenter<T : BaseView> : MvpPresenter<T>() {
+
+    protected val isViewAttached: Boolean get() = attachedViews.size > 0
+
+    private val tag = javaClass.simpleName
+
+    override fun attachView(view: T) {
+        Log.i(this.tag, "attachView")
+        super.attachView(view)
+    }
+
+    override fun detachView(view: T) {
+        Log.i(this.tag, "detachView")
+        super.detachView(view)
+    }
+}
