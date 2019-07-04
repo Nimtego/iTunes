19231e9045cb2f03e4732b1304cc7acd060ef1f5
nimtego <nimtego@gmail.com>
2019-07-04 13:09:34 +0300

    PT-107-Navigation. Add Qualifiers

---
+++ app/src/main/java/com/nimtego/plectrum/presentation/di/modules/ManagerQualifiers.kt
@@ -0,0 +1,6 @@
+package com.nimtego.plectrum.presentation.di.modules
+
+object ManagerQualifiers {
+    const val APP_CONTEXT = "APP_CONTEXT"
+    const val ACTIVITY_CONTEXT = "ACTIVITY_CONTEXT"
+}
---
+++ app/src/main/java/com/nimtego/plectrum/presentation/di/modules/navigation/NavigationQualifiers.kt
@@ -0,0 +1,9 @@
+package com.nimtego.plectrum.presentation.di.modules.navigation
+
+object NavigationQualifiers {
+    const val APP_NAVIGATION = "AppNavigation"
+    const val BOTTOM_BAR_NAVIGATION = "BottomBarNavigation"
+    const val TAB_MUSIC_NAVIGATION = "TabMusicNavigation"
+    const val TAB_MOVIE_NAVIGATION = "TabMovieNavigation"
+    const val TAB_BOOK_NAVIGATION = "TabBookNavigation"
+}
